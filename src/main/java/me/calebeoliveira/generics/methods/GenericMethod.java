cb1cfe93ff348f86c9bc5816d109fe2fee8356b6
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-12-14 16:27:31 -0300

    add generic methods example

--- src/main/java/me/calebeoliveira/generics/methods/GenericMethod.java
+++ src/main/java/me/calebeoliveira/generics/methods/GenericMethod.java
@@ -5,4 +5,15 @@ public class GenericMethod {
         System.out.println("The item is: " + item.toString());
         return item;
     }
+
+    public <T,V> void printItems(T t, V v) {
+        System.out.println(t.toString());
+        System.out.println(v.toString());
+    }
+
+    public <T> void showItems(T[] items) {
+        for(T t: items) {
+            System.out.println(t);
+        }
+    }
 }
--- src/main/java/me/calebeoliveira/generics/methods/GenericMethodExample.java
+++ src/main/java/me/calebeoliveira/generics/methods/GenericMethodExample.java
@@ -3,7 +3,11 @@ package me.calebeoliveira.generics.methods;
 public class GenericMethodExample {
     public static void main(String[] args) {
         GenericMethod genericMethod = new GenericMethod();
-        System.out.println("Item is returned: " + genericMethod.showItem("Adam"));
+//        genericMethod.printItems("Adam", 3);
+//        System.out.println("Item is returned: " + genericMethod.showItem("Adam"));
 
+        Integer[] nums = {1, 2, 3, 4};
+        String[] names = {"Adam", "Ana", "Daniel", "Kevin", "Michael"};
+        genericMethod.showItems(names);
     }
 }
