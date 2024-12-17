d779928831e2556f49cab6300cde2e6a8a15c01f
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-12-17 13:44:07 -0300

    add arraydeque vs stack comparision

---
+++ src/main/java/me/calebeoliveira/collections/queue/ArrayDequeVsStack.java
@@ -0,0 +1,36 @@
+package me.calebeoliveira.collections.queue;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Stack;
+
+public class ArrayDequeVsStack {
+    public static void main(String[] args) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        long now = System.currentTimeMillis();
+
+        for (int i = 0; i < 500_000; i++) {
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            stack.pop();
+        }
+
+        System.out.println("Time taken with ArrayDeque: " + (System.currentTimeMillis() - now) + " ms");
+
+        Stack<Integer> stack2 = new Stack<>();
+        now = System.currentTimeMillis();
+
+        for (int i = 0; i < 500_000; i++) {
+            stack2.push(i);
+        }
+
+        while (!stack2.isEmpty()) {
+            stack2.pop();
+        }
+
+        System.out.println("Time taken with Stack: " + (System.currentTimeMillis() - now) + " ms");
+
+    }
+}
