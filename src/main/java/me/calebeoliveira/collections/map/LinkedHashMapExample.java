815b85394e21de948a9ce02a3470c77d4ef3ddaf
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-12-18 13:59:25 -0300

    add linked hash map example

---
+++ src/main/java/me/calebeoliveira/collections/map/LinkedHashMapExample.java
@@ -0,0 +1,27 @@
+package me.calebeoliveira.collections.map;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LinkedHashMapExample {
+    public static void main(String[] args) {
+        // there is a doubly linked list connecting the inserted items
+        // LinkedHashMaps need more memory!!
+        Map<String, Integer> map = new LinkedHashMap<>();
+
+        map.put("aaa", 1);
+        map.put("bbb", 2);
+        map.put("ccc", 3);
+        map.put("ddd", 4);
+        map.put("eee", 5);
+        map.put("fff", 6);
+        map.put("ggg", 7);
+        map.put("hhh", 8);
+        map.put("iii", 9);
+        map.put("jjj", 10);
+
+        for(String key: map.keySet()) {
+            System.out.println(key + " - " + map.get(key));
+        }
+    }
+}
